720192ddf774abd6e68743c441b0ddded5fc507c
Fornitx <49841725+Fornitx@users.noreply.github.com>
2021-08-19 19:46:42 +0300

    formatting

--- src/main/java/com/example/kafka/DemoKafkaApplication.java
+++ src/main/java/com/example/kafka/DemoKafkaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoKafkaApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(DemoKafkaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoKafkaApplication.class, args);
+    }
 }
